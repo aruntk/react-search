4b2e8252e675b3cfe1d897a37c4ebb22f9dae8e6
aruntk <aruntknambiar@gmail.com>
2019-08-24 08:41:52 +0530

    build(storybook): storybook with hello world story added

---
+++ stories/index.stories.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react'
+
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+// tslint:disable-next-line:import-name
+import Search from '../src/search'
+
+storiesOf('button', module)
+.add('with text', () => <button onClick={action('clicked')}>Hello Button</button>)
+.add('with some emoji', () => (
+  <button onClick={action('clicked')}>
+    <span role="img" aria-label="so cool">
+      😀 😎 👍 💯
+    </span>
+  </button>
+))
+
+storiesOf('Search', module)
+.add('basic', () => <Search />)
